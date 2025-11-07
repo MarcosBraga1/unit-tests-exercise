6607709c67a8feb412a73d5b2b5d2e9c28398ee0
KaykyNery <kaykynav@gmail.com>
2025-11-07 20:47:50 -0300

    feat: Adicionando teste para a criação de instâncias de Sobremesa.

--- tests/test_sobremesa.py
+++ tests/test_sobremesa.py
@@ -1,4 +1,19 @@
 from tests.fixtures import sobremesa_fixture
+from components.cardapio.sobremesa import Sobremesa
+
+def teste_criar_instancia_sobremesa():
+    """Testa se é possível criar uma instância de Sobremesa corretamente"""
+    sobremesa = Sobremesa(
+        nome="Sorvete",
+        preco=100,
+        tipo="Gelados",
+        tamanho="500ml"
+    )
+    
+    assert sobremesa._nome == "Sorvete"
+    assert sobremesa._preco == 100
+    assert sobremesa.tipo == "Gelados"
+    assert sobremesa.tamanho == "500ml"
 
 def teste_aplicar_desconto_sobremesa(sobremesa_fixture):
     sobremesa = sobremesa_fixture
